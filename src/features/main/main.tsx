9dac2c34c65d4c33f85bdfd5a616ced329e4b004
JlBestMc <jlrcmt@gmail.com>
2025-07-23 13:46:11 +0200

    main updated

--- src/features/main/main.tsx
+++ src/features/main/main.tsx
@@ -23,7 +23,7 @@ export default function Main() {
         <div>
           <img src={logo2dark} alt="Logo" className="w-64" />
         </div>
-        <div className="flex flex-col justify-center w-92 ml-5">
+        <div className="flex flex-col justify-center w-92 ml-8">
           <h1 className="text-4xl font-bold text-start bg-gradient-to-r from-blue-950 to-purple-950 bg-clip-text text-transparent">
             TMDB MovieApp
           </h1>
@@ -32,11 +32,11 @@ export default function Main() {
             watch trailers, and get the latest updates on your favorite films.
           </p>
           <div className="flex space-x-4 justify-center mt-5">
-            <Button styles="px-12" variant="primary">
-              Registrate
+            <Button styles="px-16" variant="primary">
+              Login
             </Button>
-            <Button styles="px-10" variant="secondary">
-              Iniciar Sesión
+            <Button styles="px-14 bg-black" variant="secondary">
+              Register
             </Button>
           </div>
         </div>
